6e046b4f8649ef2331b073845879ed59a88b0dd0
pchiwan <silvia.murblanch@gmail.com>
2014-07-01 00:20:05 +0200

    Update page, styles and demo

---
+++ javascripts/demo.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function () {
+
+	//stick this one to the top of the body
+	var npBody = new NProgress();
+	npBody.done(true);
+
+	var npOne = new NProgress({
+		renderOnInit: true,
+		container: '.demoone',
+		removeOnFinish: false
+	});
+
+	var npTwo = new NProgress({
+		renderOnInit: true,
+		container: '.demotwo'
+	});
+
+	var npThree = new NProgress({
+		renderOnInit: true,
+		container: '.demothree',
+		trickleRate: 0.1,
+		randomTrickle: true,
+		removeOnFinish: false
+	});
+
+	var ViewModel = {
+		npOne: npOne,
+		npTwo: npTwo,
+		npThree: npThree
+	};
+
+	//apply ko bindings
+	ko.applyBindings(ViewModel);
+
+});
